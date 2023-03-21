02b4944b2720082ee9f23112ad517d5274ac1d82
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2023-03-22 03:02:10 +0900

    modify url

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -69,7 +69,7 @@ const App: React.FC<{}> = () => {
             fullWidth
             multiline
             minRows={4}
-            helperText="➡️ If URL of current tab matches for any pattern, auto-replace is disabled. / glob patterns, one per line. (ex. http?://*.google.com/*)"
+            helperText="➡️ If URL of current tab matches for any pattern, auto-replace is disabled. / glob patterns, one per line. / ex. http?://*.kakaocorp.com/*"
             value={excludeUrlPatterns}
             onChange={handleChange}
           />
