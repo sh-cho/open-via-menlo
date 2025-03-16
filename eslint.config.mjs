1f53a878460c434717a349c63687e5815d536fda
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2025-03-16 20:31:52 +0900

    eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,120 @@
+import { defineConfig } from "eslint/config";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: fixupConfigRules(compat.extends(
+        "airbnb",
+        "prettier",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+        "plugin:import/recommended",
+    )),
+
+    plugins: {
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.webextensions,
+        },
+
+        parser: tsParser,
+    },
+
+    settings: {
+        "import/resolver": {
+            typescript: {},
+        },
+
+        "import/parsers": {
+            "@typescript-eslint/parser": [".ts", ".tsx"],
+        },
+    },
+
+    rules: {
+        "no-console": "off",
+        "no-continue": "off",
+
+        "no-use-before-define": ["error", {
+            functions: false,
+            classes: false,
+        }],
+
+        "no-plusplus": ["error", {
+            allowForLoopAfterthoughts: true,
+        }],
+
+        "no-nested-ternary": "warn",
+        "no-unused-vars": "off",
+
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            caughtErrorsIgnorePattern: "^_",
+        }],
+
+        "prefer-destructuring": "off",
+        "import/no-extraneous-dependencies": "off",
+        "import/prefer-default-export": "off",
+
+        "import/order": ["error", {
+            pathGroups: [{
+                pattern: "~/**",
+                group: "external",
+                position: "after",
+            }],
+
+            groups: [
+                "builtin",
+                "external",
+                "internal",
+                "unknown",
+                "parent",
+                "sibling",
+                "index",
+                "object",
+                "type",
+            ],
+
+            alphabetize: {
+                order: "asc",
+                caseInsensitive: true,
+            },
+
+            "newlines-between": "always",
+        }],
+
+        "import/extensions": ["error", "ignorePackages", {
+            js: "never",
+            jsx: "never",
+            ts: "never",
+            tsx: "never",
+        }],
+
+        "react/function-component-definition": ["error", {
+            namedComponents: "arrow-function",
+            unnamedComponents: "arrow-function",
+        }],
+
+        "react/jsx-filename-extension": ["error", {
+            extensions: [".js", ".jsx", ".ts", ".tsx"],
+        }],
+    },
+}]);
