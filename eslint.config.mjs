a25e20214d986e3849b54b40888d7d2bb65941e5
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2025-03-16 20:34:32 +0900

    Apply prettier for `eslint.config.mjs`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,120 +1,148 @@
-import { defineConfig } from "eslint/config";
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint/config';
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default defineConfig([{
-    extends: fixupConfigRules(compat.extends(
-        "airbnb",
-        "prettier",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/typescript",
-        "plugin:import/recommended",
-    )),
+export default defineConfig([
+  {
+    extends: fixupConfigRules(
+      compat.extends(
+        'airbnb',
+        'prettier',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/typescript',
+        'plugin:import/recommended',
+      ),
+    ),
 
     plugins: {
-        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.webextensions,
-        },
+      globals: {
+        ...globals.browser,
+        ...globals.webextensions,
+      },
 
-        parser: tsParser,
+      parser: tsParser,
     },
 
     settings: {
-        "import/resolver": {
-            typescript: {},
-        },
+      'import/resolver': {
+        typescript: {},
+      },
 
-        "import/parsers": {
-            "@typescript-eslint/parser": [".ts", ".tsx"],
-        },
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
     },
 
     rules: {
-        "no-console": "off",
-        "no-continue": "off",
+      'no-console': 'off',
+      'no-continue': 'off',
 
-        "no-use-before-define": ["error", {
-            functions: false,
-            classes: false,
-        }],
+      'no-use-before-define': [
+        'error',
+        {
+          functions: false,
+          classes: false,
+        },
+      ],
 
-        "no-plusplus": ["error", {
-            allowForLoopAfterthoughts: true,
-        }],
+      'no-plusplus': [
+        'error',
+        {
+          allowForLoopAfterthoughts: true,
+        },
+      ],
 
-        "no-nested-ternary": "warn",
-        "no-unused-vars": "off",
+      'no-nested-ternary': 'warn',
+      'no-unused-vars': 'off',
 
-        "@typescript-eslint/no-unused-vars": ["warn", {
-            argsIgnorePattern: "^_",
-            varsIgnorePattern: "^_",
-            caughtErrorsIgnorePattern: "^_",
-        }],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
 
-        "prefer-destructuring": "off",
-        "import/no-extraneous-dependencies": "off",
-        "import/prefer-default-export": "off",
+      'prefer-destructuring': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'import/prefer-default-export': 'off',
 
-        "import/order": ["error", {
-            pathGroups: [{
-                pattern: "~/**",
-                group: "external",
-                position: "after",
-            }],
-
-            groups: [
-                "builtin",
-                "external",
-                "internal",
-                "unknown",
-                "parent",
-                "sibling",
-                "index",
-                "object",
-                "type",
-            ],
-
-            alphabetize: {
-                order: "asc",
-                caseInsensitive: true,
+      'import/order': [
+        'error',
+        {
+          pathGroups: [
+            {
+              pattern: '~/**',
+              group: 'external',
+              position: 'after',
             },
+          ],
 
-            "newlines-between": "always",
-        }],
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'unknown',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
 
-        "import/extensions": ["error", "ignorePackages", {
-            js: "never",
-            jsx: "never",
-            ts: "never",
-            tsx: "never",
-        }],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
 
-        "react/function-component-definition": ["error", {
-            namedComponents: "arrow-function",
-            unnamedComponents: "arrow-function",
-        }],
+          'newlines-between': 'always',
+        },
+      ],
 
-        "react/jsx-filename-extension": ["error", {
-            extensions: [".js", ".jsx", ".ts", ".tsx"],
-        }],
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+
+      'react/jsx-filename-extension': [
+        'error',
+        {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      ],
     },
-}]);
+  },
+]);
